4eee0db1d5787e5c5439d0b0a3912a2a087574ea
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-17 15:08:27 +0100

    Exercice 05 non corrigé

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ! What's your firstname ?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What's your lastname ?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Bonjour {firstName} {lastName}, nous sommes le {DateTime.Now}. Comment allez-vous ?");
+
+
+        }
+    }
+}
